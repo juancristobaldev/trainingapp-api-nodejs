497f5bb1c0ea31c5a655e3042c10e80e4969e3a3
juancristobaldev <juancristobaldeveloper@gmail.com>
2022-08-12 22:38:09 -0400

    graphql

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 //packages
 
+
+const {buildSchema} = require('graphql')
+const {graphqlHTTP} = require('express-graphql')
+
+const {readFileSync} = require('fs')
+const {join} = require('path')
+
 const express = require('express'),
 app = express()
 
-const cors = require('cors');
-const bodyparser = require('body-parser')
-const mysql = require('mysql');
-const session = require('express-session');
+const cors = require('cors'),
+bodyparser = require('body-parser'),
+jwt = require('jsonwebtoken'),
+config = require('./configs/configs')
+mysql = require('mysql'),
+session = require('express-session');
+
+app.set('key', config.key)
 
 //env
 
@@ -25,15 +36,22 @@ app.use(session({
 
 }));
 
-//conexion a la bd
+const resolvers = require('./lib/resolvers')
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-
-connection.connect( (err) => {
-    if(err) throw err
-    else console.log('Conectado a a la base de datos')
-    }
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    )
 )
+
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue:resolvers,
+    graphiql:true,
+
+}))
 
 // port
 
@@ -44,170 +62,6 @@ const port = process.env.PORT;
 app.get( '/', (req,res) => {
     res.send('Api funcionando')
 })
-
-//SELECCIONAR TABLA DE DATOS
-
-app.get('/api/select/:table/', async (req,res) => { // SELECCIONAR TABLA
-    connection.query(`SELECT * FROM ${req.params.table}`, (err,result) => {
-        if (err) throw err
-        else res.send(result)
-    })
-});
-
-//SELECCIONAR ELEMENTO ATRAVEZ DE USUARIO EN UNA TABLA DE DATOS
-
-app.get('/api/select/:table/user/:userid' , async (req,res) => { 
-    await connection.query(`SELECT * FROM ${req.params.table} WHERE usuario = ${req.params.userid}`,
-    (err,result) => {
-        if(err) throw err
-        else res.send(result)
-    })
-
-})
-
-//SELECCIONAR ELEMENTO ATRAVEZ DE ID DE ELEMENTO EN UNA TABLA DE DATOS
-
-app.get('/api/select/:table/element/:id', async (req,res) =>{ // SELECCIONAR USER POR ID
-    await connection.query(`SELECT * FROM ${req.params.table} WHERE id = ${req.params.id}`,(err,result)=>{
-        if(err) throw err
-        else res.send(result)
-    })
-})
-
-app.post( '/api/users/create-user' , async  (req, res) => {
-    const userData = {
-        name:req.body.name,
-        user:req.body.user,
-        email:req.body.email,
-        date:req.body.date,
-        pass:req.body.pass,
-        passConfirm:req.body.passConfirm
-    }
-
-    const sqlSelect = 'SELECT * FROM users';
-    connection.query( sqlSelect, (err,users) => {
-        const {user,name,email,date,pass } = userData
-
-        const errors = []
-
-        users.forEach(item => {
-            if(item.user === user) errors.push('Este usuario ya existe. Selecciona otro por favor');
-            if(item.email === email) errors.push('Este email ya esta en uso, por favor escribe uno nuevo');
-        })
-
-        if(errors.length > 0) res.send(JSON.stringify({error:true, errors:errors}))
-        else{
-            const sqlInsert = "INSERT INTO users (user,name,email,date,pass) VALUES ?"
-            const values = [
-                [user,name,email,date,pass]
-            ]
-             connection.query(sqlInsert, [values], (err,result) => {
-                if (err) console.log(err)
-                else res.send({error:false})
-            })
-        }
-    })
-})
-
-// AUTH
-
-app.post( '/api/auth' , (req, res) => {
-
-    const formData = {
-        user:req.body.user,
-        pass:req.body.pass
-    }
-    const { user,pass } = formData
-    const sqlAuth = `SELECT * FROM users WHERE user = ? AND pass = ?`
-
-    connection.query(sqlAuth, [user,pass], (err,find) => {
-
-        if(err) throw err;
-        if(find.length > 0){
-            req.session.userID = find[0].id
-            res.send(JSON.stringify([{key:find[0].id}]));
-        }else{
-            res.send(JSON.stringify([{error:true, message: 'Usuario y/o contraseña incorrecto'}]));
-        }
-    })
-
-})
-
-// EXERCISES
-
-// CREATE EXERCISE
-
-app.post( '/api/exercises/create-exercise' , (req,res) => {
-    const dataForm = {
-        idUser:req.body.id,
-        name:req.body.name,
-        muscle:req.body.muscle,
-        type:req.body.type,
-        series:[]
-    }
-
-    const {idUser,name,muscle,type,series} = dataForm;
-
-    const sqlInsert = 'INSERT INTO exercises (usuario,nameEx,typeEx,muscleEx,seriesEx) VALUES ?'
-    const values = [
-        [idUser,name,type,muscle,JSON.stringify(series)]
-    ]
-
-    if(name.length > 0){
-        connection.query(sqlInsert,[values],(err,sucess) => {
-            if(err) throw err
-            else {
-                connection.query('SELECT * FROM exercises WHERE usuario = ?',[idUser], (err,result) => {
-                    res.send(result)
-                })
-            }
-        })
-    }else{
-        res.send(false)
-    }
-})
-
-// DELETE EXERCISE 
-
-app.post( '/api/exercises/delete-exercise/:id' , async (req,res) => {
-    const dataForm = {
-        idUser:parseInt(req.params.id),
-        items:req.body
-    }
-
-    const {idUser,items} = dataForm;
-
-    await items.forEach(item => {
-        const sqlDelete = `DELETE FROM exercises WHERE nameEx = '${item.nameEx}' AND usuario = '${idUser}'`;
-        connection.query(sqlDelete, (err,done) => {
-            if(err) throw err;
-        })
-    })
-
-    res.send(true)
-})
-
-//ROUTINES 
-
-app.post( '/api/routines/create-routine/' , async (req,res) => {
-    const dataForm = {
-        routine:req.body
-    }
-
-    const {done,timeRecord,exercises,nameRoutine,idUser } = dataForm.routine
-    const values = [
-        [idUser,nameRoutine,done,timeRecord,JSON.stringify(exercises)]
-    ]
-    
-    console.log(exercises)
-
-    connection.query('INSERT INTO routines (usuario,name,dones,timeRecord,exercises) VALUES ?',[values], (err,done) => {
-        if(err) throw err
-        if(done) console.log('good')
-    })
-
-})
-
 
 app.listen(port,() => {
     console.log('Corriendo puerto en puerto ' + port)
