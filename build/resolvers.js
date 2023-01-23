a65b2b6b8e56c98e632127f54d130b2c6e1f1c61
juancristobaldev <juancristobaldeveloper@gmail.com>
2023-01-22 22:41:22 -0300

    fix

--- build/resolvers.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const mutations = require('./mutations/mutations');
-const queries = require('./queries/queries');
-module.exports = Object.assign(Object.assign({}, queries), mutations);
