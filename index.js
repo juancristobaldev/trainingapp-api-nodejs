dc4acf28bb1d6120058b7dc0b068099480ad0941
juancristobaldev <juancristobaldeveloper@gmail.com>
2023-05-20 16:46:39 -0400

    fix

--- index.js
+++ index.js
@@ -1,39 +1,82 @@
 //packages
 
-const express = require('express')
-const { buildSchema } = require('graphql')
-const { graphqlHTTP } = require('express-graphql') 
-const { readFileSync } = require('fs')
-const { join } = require('path')
+const express = require("express");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const { initPassport } = require("./lib/passport/initPassport");
 
+const session = require("express-session");
 
-
+const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 //env
 
-require('dotenv').config({path:'./.env'})
-
-
+require("dotenv").config({ path: "./.env" });
 
 app.use(cors());
 
-const resolvers = require('./lib/resolvers');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const resolvers = require("./lib/resolvers");
+const passport = require("passport");
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SECRET_EXPRESS_SESSION,
+  })
+);
 
 const schema = buildSchema(
-    readFileSync(
-        join(__dirname,'/lib/', 'schema.graphql'),
-        'utf-8'
-    )
-)
+  readFileSync(join(__dirname, "/lib/", "schema.graphql"), "utf-8")
+);
 
-
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    rootValue:resolvers,
-    graphiql:true,
-}))
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
+
+initPassport(app);
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    session: true,
+  }),
+  (req, res) => {
+    console.log(req.user);
+
+    res.redirect(`/success?response=${JSON.stringify(req.user)}`);
+  }
+);
+
+app.get("/auth/google", passport.authenticate("google"));
+
+app.get(
+  "/oauth2/redirect/google",
+  passport.authenticate("google", {
+    session: true,
+  }),
+  (req, res) => {
+    res.redirect(`/success?response=${JSON.stringify(req.user)}`);
+  }
+);
+
+app.get("/success", (req, res) => {
+  res.send({ success: "true" });
+});
 
 // port
 
@@ -41,12 +84,11 @@ const port = process.env.PORT;
 
 // routes
 
-app.get( '/', (req,res) => {
-    res.send('Welcome to my Api in Vercel 🚀 ')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to my Api in Vercel 🚀 ");
+});
 
 // server
-app.listen( port, () => {
-    console.log(`🚀 Server running at: ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`🚀 Server running at: ${port}`);
+});
